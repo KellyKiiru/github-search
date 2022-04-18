dca21d283f7f969a816077ee4b1b06be16804e60
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-18 23:28:47 +0300

    modify api service

--- src/app/get-api.service.ts
+++ src/app/get-api.service.ts
@@ -1,36 +1,73 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { lastValueFrom, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GetApiService {
-  private username: string | undefined;
+
+  username = 'KellyKiiru';
+
+  base_url = 'https://api.github.com'
+
+  async searchUsers(username:string){
+    const search = this.http.get<any>(`${this.base_url}/users/${username}`)
+    return await lastValueFrom(search).then((response)=> response)
+  }
+
+  getUserRepo(username:string):Observable<any>{
+    return this.http.get<any>(`${this.base_url}/users/${username}/repos`)
+  }
+
+
   constructor(private http: HttpClient) {}
 
-  searchUsers(name: string) {
-    return this.http.get<any>('https://api.github.com/search/users?q=' + name, {
-      observe: 'response',
-    });
-  }
-
-  getProfile(name: string) {
-    return this.http.get<any>('https://api.github.com/users/' + name, {
-      observe: 'response',
-    });
-  }
-
-  getUserRepos(name: string) {
-    return this.http.get<any>(
-      'https://api.github.com/users/' + name + '/repos?per_page=150',
-      { observe: 'response' }
-    );
-  }
-
-  getContributors(name: string, repo: string) {
-    return this.http.get<any>(
-      'https://api.github.com/repos/' + name + '/' + repo + '/contributors',
-      { observe: 'response' }
-    );
-  }
+ 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // searchUsers(name: string) {
+  //   return this.http.get<any>('https://api.github.com/search/users?q=' + name, {
+  //     observe: 'response',
+  //   });
+  // }
+
+  // getProfile(name: string) {
+  //   return this.http.get<any>('https://api.github.com/users/' + name, {
+  //     observe: 'response',
+  //   });
+  // }
+
+  // getUserRepos(name: string) {
+  //   return this.http.get<any>(
+  //     'https://api.github.com/users/' + name + '/repos?per_page=150',
+  //     { observe: 'response' }
+  //   );
+  // }
+
+  // getContributors(name: string, repo: string) {
+  //   return this.http.get<any>(
+  //     'https://api.github.com/repos/' + name + '/' + repo + '/contributors',
+  //     { observe: 'response' }
+  //   );
+  // }
+
