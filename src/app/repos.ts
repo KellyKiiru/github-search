08229cf70944a135cc911cd1504932c3f2bf310b
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-19 01:13:22 +0300

    add ngFor to users.component

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,2 +1,11 @@
 export class Repos {
+    constructor(public name:string,
+         public html_url:string, 
+         public description:string,
+         public created_at:Date,
+         public fork_count:any, 
+         public stargazers_count:any,
+         public language:string){}
 }
+
+
