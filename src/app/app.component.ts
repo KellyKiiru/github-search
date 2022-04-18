7594d51e075720a8b6900f977c3f0c425ae67ddd
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-18 14:50:55 +0300

    add method to get repos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit,HostListener } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { GetApiService } from './get-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'github-search';
@@ -19,44 +19,53 @@ export class AppComponent implements OnInit {
   open = false;
 
   @HostListener('document:click', ['$event']) onClick(event: any) {
-
     if (event.target.attributes.id) {
       if (event.target.attributes.id.nodeValue === 'found') {
         this.open = true;
       } else {
         this.open = false;
       }
-    }else{
+    } else {
       this.open = false;
     }
-
   }
 
-  constructor(public getApiService: GetApiService){
-
-  }
+  constructor(public getApiService: GetApiService) {}
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
   searchUsers() {
-    this.getApiService.searchUsers(this.username.split(" ").join("")).subscribe(
-      res => {
-
+    this.getApiService.searchUsers(this.username.split(' ').join('')).subscribe(
+      (res) => {
         console.log(res);
 
         this.usersList = res.body.items;
-
-
       },
       (error) => {
         this.error = true;
-
-      });
-
-
-
+      }
+    );
   }
 
-  
+  getUser() {
+    this.loading = true;
+
+    this.getApiService.getProfile(this.username.split(' ').join('')).subscribe(
+      (res) => {
+        this.profile = res.body;
+        this.getRepos();
+
+        this.loading = false;
+        this.error = false;
+      },
+      (error) => {
+        this.error = true;
+      }
+    );
+  }
+
+  getRepos(): void {
+    
+  }
 }
