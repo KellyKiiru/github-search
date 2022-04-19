58a02c4fc2183c9712de34c80776f1f3d310ae2d
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-19 04:34:10 +0300

    add pipe and figure some bugs

---
+++ src/app/timer.pipe.ts
@@ -0,0 +1,15 @@
+// import { Pipe, PipeTransform } from '@angular/core';
+
+// @Pipe({
+//   name: 'timer'
+// })
+// export class TimerPipe implements PipeTransform {
+//   transform(value: any, ...args: any[]) {
+//     throw new Error('Method not implemented.');
+//   }
+
+//   // transform(value: number, ...args?:number): any {
+//   //   return null;
+//   // }
+
+// }
